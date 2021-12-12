f9f7b2a002a6a7abab1e860488c1f60db9ae73cb
tamashirotokiya <s21017@std.it-college.ac.jp>
2021-12-12 05:05:48 +0000

    dekimasita

--- src/hoge.js
+++
@@ -1,8 +0,0 @@
-const [data, setData] = useState({})
-const uri = 'https://opendata.corona.go.jp/api/Covid19JapanAll?date=20201125'
-useEffect(() => (
-  window.fetch(uri)
-    .then(res => res.json())
-    .then(console.log)
-
-), [])
